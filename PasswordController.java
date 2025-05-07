7bc9ba657e37b6243f6112ead5c22bda0938bf27
Rodrigo Freitas <164232597+rodrigosfreita@users.noreply.github.com>
2025-05-07 00:14:03 -0300

    Add files via upload

--- PasswordController.java
+++ PasswordController.java
@@ -5,8 +5,7 @@ import com.example.password.service.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
